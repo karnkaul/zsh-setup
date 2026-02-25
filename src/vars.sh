e69a12505c8a117ba387f650c773e1665d3627a7
Karn Kaul <karnkaul@gmail.com>
2026-02-25 09:00:16 -0800

    [v1.3] Use `XDG_DATA_DIR` for theme

--- src/vars.sh
+++ src/vars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tool_version=1.2
+tool_version=1.3
 
-package_list="\
+zsh_packages="\
   zsh\
   zsh-autocomplete\
   zsh-autosuggestions\
@@ -15,8 +15,10 @@ font_package=ttf-hack-nerd
 p10k_repo_name=powerlevel10k
 p10k_repo_url=https://github.com/romkatv/$p10k_repo_name
 
-zsh_themes_dir="$HOME/zsh_themes"
-p10k_dir="$zsh_themes_dir/$p10k_repo_name"
+config_dir=${XDG_DATA_DIR:-"$HOME/.local/share"}
+p10k_dir="${config_dir}/zsh-theme-powerlevel10k"
+p10k_theme="${p10k_dir}/powerlevel10k.zsh-theme"
+
 zshrc_path="$HOME/.zshrc"
 
 echo "-- zsh-setup v$tool_version"
